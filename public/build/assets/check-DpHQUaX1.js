4cc604cd38c18277c020e3ced456d13d07934300
JosephNjoroge8 <joem7mach@gmail.com>
2025-12-06 16:09:30 +0300

    Build production assets for deployment

---
+++ public/build/assets/check-DpHQUaX1.js
@@ -0,0 +1,1 @@
+import{c}from"./app-D60y3zmh.js";const p=c("check",[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]]);export{p as C};
---
+++ public/build/assets/plus-DT76qllc.js
@@ -0,0 +1,1 @@
+import{c as p}from"./app-D60y3zmh.js";const s=p("plus",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]]);export{s as P};

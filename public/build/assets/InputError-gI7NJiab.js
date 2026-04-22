0c72be2786f776090c677b4ea821e843625cad88
JosephNjoroge8 <joem7mach@gmail.com>
2026-04-23 00:03:54 +0300

    Update production build assets - fix blank page issue

---
+++ public/build/assets/InputError-gI7NJiab.js
@@ -0,0 +1,1 @@
+import{j as s}from"./app-CnXB4r_e.js";function e({message:e,className:a="",...t}){return e?s.jsx("p",{...t,className:"text-sm text-red-600 "+a,children:e}):null}export{e as I};
--- public/build/assets/loader-circle-D4vcbJF7.js
+++
@@ -1,1 +0,0 @@
-import{c as a}from"./app-DchzgzFs.js";const o=a("loader-circle",[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]]);export{o as L};
